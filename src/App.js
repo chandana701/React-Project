d92cea802d78c33e6f158071ce579b14d75dfbc5
chandana701 <mounikadcs@gmail.com>
2020-05-12 19:37:46 +0530

    ParentChildComponents

--- src/App.js
+++ src/App.js
@@ -9,80 +9,26 @@ import React from 'react';
 
     constructor(props){
       super(props);
-      this.state = {
-        isLoggedIn :true,
-        username : '',
-      }
+      
+     
     }
-    Lg=(event)=>{
-      // event.preventDefault();
-      this.setState({
-        isLoggedIn : true
-      })
-    }
-    LO= (event)=>{
-      this.setState({
-        isLoggedIn : false
-      })
-    }
-    myChangeHandler = (event) =>{
-      this.setState({
-        username : event.target.value,
-        isLoggedIn :true
-      })
-
-    }
-
+   
     render(){
-      return(
+      return (
       
-       <form>
-   
       
-        {this.state.isLoggedIn?
-        <p>
-        <h1>User Name</h1>
-       <input type = 'text' 
-          name = 'username'
-          onChange = {this.myChangeHandler}
-       />
+       <div>
+      
+       </div>
         
-          <button onClick={(event)=>{event.preventDefault();this.LO()}}>Login</button>
-          
-          </p>
-          :
-         <p>
-           <div>Welcome {this.state.username}!</div>
-          <button onClick={(event)=>{event.preventDefault();this.Lg()}}>Logout</button>
-   
-        </p>
-        }
-         
-        
-      
-       
-       {/* {this.state.isLoggedIn?
-        <Login 
-          lo = {this.LO}
-        />    
-           :
-        <Logout
-          lg = {this.Lg}
-        />      
-       
-       } */}
-     
-
-     
-       </form>
-        
-      
-      
       );
+      
+      
+      
    
     }
 
-
+      
 
 
 
